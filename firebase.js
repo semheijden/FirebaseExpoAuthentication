31f7a9a5f9c2753ce09ea258555a9608eaec06f5
Sem van der Heijden <semvanderheijden03@gmail.com>
2023-05-09 21:57:04 +0200

    test
    test

--- firebase.js
+++ firebase.js
@@ -1,26 +1,21 @@
 // Import the functions you need from the SDKs you need
-import * as firebase from "firebase";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCLA4vnLFaVow25ylLUkb57NhmnN6qk1vE",
-  authDomain: "fir-auth-65294.firebaseapp.com",
-  projectId: "fir-auth-65294",
-  storageBucket: "fir-auth-65294.appspot.com",
-  messagingSenderId: "105428798939",
-  appId: "1:105428798939:web:49a9375f32ea94d4dfebfe"
+  apiKey: "AIzaSyARa0VFTLC-FR_XBH14wVQ2JEVhQczgP4Y",
+  authDomain: "diary-fd993.firebaseapp.com",
+  projectId: "diary-fd993",
+  storageBucket: "diary-fd993.appspot.com",
+  messagingSenderId: "77435039072",
+  appId: "1:77435039072:web:eb570d81a3d69a832a0205",
+  measurementId: "G-JXXR722042"
 };
 
 // Initialize Firebase
-let app;
-if (firebase.apps.length === 0) {
-  app = firebase.initializeApp(firebaseConfig);
-} else {
-  app = firebase.app()
-}
-
-const auth = firebase.auth()
-
-export { auth };
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
